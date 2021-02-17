9dc408619244438e301b65dacc79f160a6409406
Marco Matildo <marcomatildo@gmail.com>
2021-02-18 07:50:16 +0800

    added new component to page :sparkles:

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-import SearchBar from "../components/SearchBar";
 import Meals from "../components/Meals";
 import Preloader from "../components/Preloader";
 import { getMeals } from "../api";
 import PopularIngredients from "../components/PopularIngredients";
+import { latestMeals as latestFood } from "../api";
 
 const Home = () => {
-  const [search, setSearch] = useState("");
   const [meals, setMeals] = useState([]);
-  const [hasSearch, setHasSearch] = useState(false);
+  const [latestMeals] = useState(latestFood);
 
   useEffect(() => {
     const results = async () => {
@@ -21,9 +20,23 @@ const Home = () => {
 
   return (
     <div>
-      <SearchBar search={search} setSearch={setSearch} />
-      {meals.length !== 8 ? <Preloader /> : <Meals meals={meals} />}
+      <div className="latestmeals-section center-align">
+        <h4 className="brown-text text-darken-4">
+          <span>Latest Meals</span>
+        </h4>
+        <Meals meals={latestMeals} />
+      </div>
       <PopularIngredients />
+      {meals.length !== 8 ? (
+        <Preloader />
+      ) : (
+        <>
+          <h4 className="brown-text text-darken-4 center-align">
+            <span>Random Meals</span>
+          </h4>
+          <Meals meals={meals} />
+        </>
+      )}
     </div>
   );
 };
