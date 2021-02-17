89201f5045213e391d2236c3581b84ed3825c946
Marco Matildo <marcomatildo@gmail.com>
2021-02-18 07:47:51 +0800

    json data for latest meals :zap:

--- src/api/index.js
+++ src/api/index.js
@@ -10,6 +10,57 @@ const API = {
   Image: "https://www.themealdb.com/images/ingredients",
 };
 
+const latestMeals = [
+  {
+    idMeal: "53038",
+    strMeal: "Mustard champ",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/o7p9581608589317.jpg",
+  },
+  {
+    idMeal: "53037",
+    strMeal: "Coddled pork with cider",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/7vpsfp1608588991.jpg",
+  },
+  {
+    idMeal: "53036",
+    strMeal: " Boxty Breakfast",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/naqyel1608588563.jpg",
+  },
+  {
+    idMeal: "53035",
+    strMeal: "Ham hock colcannon",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/n41ny81608588066.jpg",
+  },
+  {
+    idMeal: "53034",
+    strMeal: "Japanese Katsudon",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/d8f6qx1604182128.jpg",
+  },
+  {
+    idMeal: "53033",
+    strMeal: "Japanese gohan rice",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/kw92t41604181871.jpg",
+  },
+  {
+    idMeal: "53032",
+    strMeal: "Tonkatsu pork",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/lwsnkl1604181187.jpg",
+  },
+  {
+    idMeal: "53031",
+    strMeal: "Egyptian Fatteh",
+    strMealThumb:
+      "https://www.themealdb.com/images/media/meals/rlwcc51598734603.jpg",
+  },
+];
+
 // search meal name
 const mealByName = async () => {
   const res = await fetch(API.searchByName);
@@ -47,5 +98,5 @@ const getAreas = async () => {
   return data.meals;
 };
 
-export { mealByName, getMeals, getAreas, mealDetails };
+export { mealByName, getMeals, getAreas, mealDetails, latestMeals };
 export default API;
