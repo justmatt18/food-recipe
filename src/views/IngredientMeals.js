87a0ccefc615c809489bdcffd05a02f4ddb6cb8a
Marco Matildo <marcomatildo@gmail.com>
2021-02-18 07:49:40 +0800

    added new pages :sparkles:

---
+++ src/views/IngredientMeals.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const IngredientMeals = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col s12 center-align">Ingredient</div>
+        <p>Ing Image</p>
+        <p>Ing Name -</p>
+      </div>
+      <div className="row">
+        <p className="center-align">
+          <span>Meals</span>
+        </p>
+        <div className="col s12 m4 l3">Card Card Meal</div>
+      </div>
+    </div>
+  );
+};
+
+export default IngredientMeals;
