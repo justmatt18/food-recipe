89fa6974cfd82bbece42697dbf589a0eebb5c34a
Marco Matildo <marcomatildo@gmail.com>
2021-02-18 09:07:30 +0800

    Update SearchBar :sparkles:

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,30 +12,31 @@ const SearchBar = () => {
   return (
     <nav>
       <div className="nav-wrapper brown darken-2">
-        <div className="row container">
-          <div className="col s10 m6">
-            <form className="search-form" onSubmit={toSearchPage}>
-              <div className="input-field">
-                <input
-                  id="search"
-                  type="search"
-                  placeholder="Search for a Meal..."
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                />
-                <label className="label-icon">
-                  <i className="material-icons">search</i>
-                </label>
-                <i className="material-icons">close</i>
-              </div>
-            </form>
-          </div>
-          <div className="col s2 m6 ">
-            <ul id="nav-mobile" class="right ">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-            </ul>
+        <div className="container">
+          <div className="row">
+            <div className="col s10">
+              <form>
+                <div className="input-field">
+                  <input
+                    id="search"
+                    type="search"
+                    required
+                    placeholder="Search for a Meal..."
+                  />
+                  <label className="label-icon" for="search">
+                    <i className="material-icons">search</i>
+                  </label>
+                  <i className="material-icons">close</i>
+                </div>
+              </form>
+            </div>
+            <div className="col s2">
+              <ul id="nav-mobile" class="right ">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
