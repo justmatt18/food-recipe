023b95632152e3a8ce1f282bb05cdf06588b9816
Marco Matildo <marcomatildo@gmail.com>
2021-02-27 08:16:18 +0800

    added content for ingredients and meals :sparkles:

--- src/views/IngredientMeal.js
+++ src/views/IngredientMeal.js
@@ -1,18 +1,95 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import {
+  listOfIngredients,
+  getIngredientImg,
+  filterByMainIngredient,
+} from "../api";
+import Meals from "../components/Meals";
+import Preloader from "../components/Preloader";
 
 const IngredientMeal = () => {
+  const [meals, setMeals] = useState([]);
+  const [ingredient, setIngredient] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    getIngredients();
+    // setIngredientId(id);
+    fetchMealsByIngredient();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id, ingredient]);
+
+  const getIngredients = async () => {
+    let data = await listOfIngredients();
+    setIngredient(data[id - 1]);
+  };
+
+  const fetchMealsByIngredient = async () => {
+    const data = await filterByMainIngredient(ingredient.strIngredient);
+    console.table(data);
+    setMeals(data);
+  };
+
+  const isEmpty = (obj) => {
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) return false;
+    }
+    return true;
+  };
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col s12 center-align">Ingredient</div>
-        <p>Ing Image</p>
-        <p>Ing Name -</p>
+    <div className="container brown-text text-darken-2 mb-3">
+      <div className="margin-tb row  mb-3 meal-info">
+        {!isEmpty(ingredient) ? (
+          <div>
+            <div className="col s12 m4 mb-3">
+              <h4 className="center-align">
+                <span>{ingredient.strIngredient}</span>
+              </h4>
+              <img
+                className="materialboxed responsive-img"
+                src={getIngredientImg(ingredient.strIngredient)}
+                alt={`${ingredient.strIngredient} meal`}
+              />
+
+              <ul className="pagination center-align">
+                <li className="waves-effect">
+                  <Link to={`/ingredients/${1 * id - 1}`}>
+                    <i className="material-icons">chevron_left</i>
+                  </Link>
+                </li>
+                <li className="waves-effect">
+                  <Link to={`/ingredients/${1 * id + 1}`}>
+                    <i className="material-icons">chevron_right</i>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col s12 m7 push-m2 mb-3">
+              <h4 className="center-align mb-3">
+                <span>Description</span>
+              </h4>
+              <p>
+                <span>{ingredient.strDescription}</span>
+              </p>
+            </div>
+          </div>
+        ) : (
+          <div className="center-align">
+            <Preloader />
+          </div>
+        )}
       </div>
       <div className="row">
-        <p className="center-align">
-          <span>Meals</span>
-        </p>
-        <div className="col s12 m4 l3">Card Card Meal</div>
+        <div className="col s12">
+          <h4 className="center-align">
+            <span>Ingredients</span>
+          </h4>
+          <div className="center-align">
+            {!isEmpty(meals) ? <Meals meals={meals} /> : <Preloader />}
+          </div>
+        </div>
       </div>
     </div>
   );
